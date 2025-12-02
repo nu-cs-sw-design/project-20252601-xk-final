58fc0baa21633b80fa407e9081bbc9987a949b12
shadekitty13 <shadekittyofficial@gmail.com>
2025-12-02 15:53:38 -0600

    Switched to using asm tree API for stylecheck, made method visiting functionality work Fixed and updated design diagram

--- src/main/java/domain/stylecheck/ClassStyleRevisionManager.java
+++ src/main/java/domain/stylecheck/ClassStyleRevisionManager.java
@@ -1,25 +1,23 @@
 package domain.stylecheck;
 
 import org.jetbrains.annotations.Nullable;
-import org.objectweb.asm.ClassVisitor;
-import org.objectweb.asm.FieldVisitor;
-import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldNode;
+import org.objectweb.asm.tree.InnerClassNode;
+import org.objectweb.asm.tree.MethodNode;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.objectweb.asm.Opcodes.ASM7;
-
-public class StyleRevisionClassVisitor extends ClassVisitor {
+public class ClassStyleRevisionManager {
 
     private List<ClassStyleReviser> revisers = new ArrayList<>();
     private final StyleRevisionPrinter revisionPrinter;
 
-    public StyleRevisionClassVisitor(OutputStream output) {
-        super(ASM7);
+    public ClassStyleRevisionManager(OutputStream output) {
         this.revisionPrinter = new BasicStyleRevisionPrinter(output);
     }
 
@@ -31,14 +29,22 @@ public class StyleRevisionClassVisitor extends ClassVisitor {
         revisers.remove(reviser);
     }
 
-    @Override
-    public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+    public void revise(ClassNode classNode) {
+
+        reviseClassName(classNode.name);
+        classNode.fields.forEach(this::reviseField);
+        classNode.methods.forEach(this::reviseMethod);
+        classNode.innerClasses.forEach(this::reviseInnerClass);
+
+        printRevisions();
+
+    }
+
+    private void reviseClassName(String name) {
         revisers.forEach(reviser -> reviser.checkClassName(getLocalClassName(name)));
     }
 
-    @Override
-    @Nullable
-    public FieldVisitor visitField(int access, String name, String descriptor, String signature, Object value) {
+    private void reviseField(FieldNode field) {
 
         // Define list of access constants that should use the static constant check
         List<Integer> constantFieldOpcodes = List.of(
@@ -49,30 +55,34 @@ public class StyleRevisionClassVisitor extends ClassVisitor {
         );
 
         // Do appropriate style check
-        if (constantFieldOpcodes.contains(access)) {
-            revisers.forEach(reviser -> reviser.checkStaticConstantName(name));
+        if (constantFieldOpcodes.contains(field.access)) {
+            revisers.forEach(reviser -> reviser.checkStaticConstantName(field.name));
         }
         else {
-            revisers.forEach(reviser -> reviser.checkVariableOrMethodName(name));
+            revisers.forEach(reviser -> reviser.checkVariableOrMethodName(field.name));
         }
 
-        return null;
     }
 
-    @Override
-    @Nullable
-    public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
-        revisers.forEach(reviser -> reviser.checkVariableOrMethodName(name));
-        return null;
+    private void reviseMethod(MethodNode method) {
+
+        revisers.forEach(reviser -> reviser.checkVariableOrMethodName(method.name));
+
+        if (method.parameters != null)
+            method.parameters.forEach(parameterNode -> revisers.forEach(reviser -> reviser.checkVariableOrMethodName(parameterNode.name)));
+
+        if (method.localVariables != null)
+            method.localVariables.forEach(localVariableNode -> revisers.forEach(reviser -> reviser.checkVariableOrMethodName(localVariableNode.name)));
+
     }
 
-    @Override
-    public void visitInnerClass(String name, String outerName, String innerName, int access) {
-        revisers.forEach(reviser -> reviser.checkClassName(getLocalClassName(name)));
+    private void reviseInnerClass(InnerClassNode innerClass) {
+
+        reviseClassName(innerClass.name);
+
     }
 
-    @Override
-    public void visitEnd() {
+    private void printRevisions() {
         for (ClassStyleReviser reviser : revisers) {
             List<StyleRevision> revisions = reviser.getRevisions();
             revisions.forEach(revision -> {
